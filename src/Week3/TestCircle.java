aed333270ff6849b3d44ba9bcc7e4e87091421b0
Will <w.stolton@gmail.com>
2019-10-04 01:17:00 +0100

    week 3 started

---
+++ src/Week3/TestCircle.java
@@ -0,0 +1,12 @@
+package Week3;
+
+public class TestCircle {
+
+    public static void main(String[] args){
+        Circle c1 = new Circle();
+        System.out.println("The circle has a radius of: " + c1.getRadius() + ", and an area of: " + c1.getArea());
+
+        Circle c2 = new Circle(2.0);
+        System.out.println("The circle has a radius of: " + c2.getRadius() + ", and an area of: " + c2.getArea());
+    }
+}
---
+++ src/Week3/TestCylinder.java
@@ -0,0 +1,34 @@
+package Week3;
+
+public class TestCylinder {
+
+    public static void main(String[] args){
+        Cylinder c1 = new Cylinder();
+        System.out.println("Cylinder radius: " + c1.getRadius() +
+                "\nCylinder height: " + c1.getHeight() +
+                "\nCylinder area: " + c1.getArea() +
+                "\nCylinder volume: " + c1.getVolume() +
+                "\nCylinder colour: " + c1.getColor());
+
+        Cylinder c2 = new Cylinder(2.0);
+        System.out.println("\nCylinder radius: " + c2.getRadius() +
+                "\nCylinder height: " + c2.getHeight() +
+                "\nCylinder area: " + c2.getArea() +
+                "\nCylinder volume: " + c2.getVolume() +
+                "\nCylinder colour: " + c2.getColor());
+
+        Cylinder c3 = new Cylinder(34.0, 3.0);
+        System.out.println("\nCylinder radius: " + c3.getRadius() +
+                "\nCylinder height: " + c3.getHeight() +
+                "\nCylinder area: " + c3.getArea() +
+                "\nCylinder volume: " + c3.getVolume() +
+                "\nCylinder colour: " + c3.getColor());
+
+        Cylinder c4 = new Cylinder(6.0, 10.0, "blue");
+        System.out.println("\nCylinder radius: " + c4.getRadius() +
+                "\nCylinder height: " + c4.getHeight() +
+                "\nCylinder area: " + c4.getArea() +
+                "\nCylinder volume: " + c4.getVolume() +
+                "\nCylinder colour: " + c4.getColor());
+    }
+}
