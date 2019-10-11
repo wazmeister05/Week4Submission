5906b2a12fc31c2611a203152f672ad74d072476
Will <w.stolton@gmail.com>
2019-10-11 01:05:04 +0100

    Week 3 complete

--- src/Week1/Birthday.java
+++ src/Week1/Birthday.java
@@ -2,23 +2,37 @@ package Week1;
 
 public class Birthday {
 
-        public void run(int sampleSize){
+        public void run(int sampleSize) {
 
-            double notSameChance = 1.0;
-            for (int i = 0; i < sampleSize; i++)
-                notSameChance *= (365-i) / 365.0;
-            System.out.println("\nBased on a sample size of "+sampleSize);
-            System.out.printf("Not Same Birthday chance: %.0f%%\n",
-                    (notSameChance * 100) );
-            System.out.printf("Same Birthday chance: %.0f%%\n",
-                    ((1 - notSameChance) * 100)  );
+            //The sum for this is -----   365! / ((365-n)! * 365^n).
+            //trying the below however, just produces NaN because the numbers are just too ridiculous.
+            //System.out.println(factorial(365) / (factorial(365-i) * Math.pow(365,sampleSize)));
+
+            //another sum for this is --------   1-(364/365)*(363/365)...((365-n)/365).
+            //according to https://www.dcode.fr/birthday-problem
+            //this led me to the following, stupidly short solution, compared to how I felt when I first read the problem.
+
+            double test = 1;
+            for (int i = 0; i < sampleSize; i++) {
+                test = test * (365 - i) / 365;
+            }
+            test = (1 - test) * 100;
+            System.out.println("\nBased on a sample size of " + sampleSize);
+            System.out.printf("%.2f", test);
+            System.out.println("% chance of sharing a birthday");
+
         }
+
+
+            //program I made to calculate a factorial. It didn't turn out to be useful
+            // but I'm leaving it here because I'm proud of it.
+            //n x n-1 x n-2 etc
+            //sadly, to use it to find the birthday paradox, double is too small.
+            public double factorial(double max){
+                double maximum = max;
+                for(int i = 1; i < max; i++){
+                   maximum = maximum * (max - i);
+                }
+                return maximum;
+            }
 }
-
-/*
-     364!/(342! * 365^22) = 0.492703 = 49.3% chance of not sharing a birthday
-     1 - 0.493 * 100 = 50.7%   chance of sharing a birthday
-
-     364!/(342! * 365^100) = 0.001 chance of not sharing a birthday
-     1 - 0.001 * 100 = 99.9% chance of sharing a birthday
- */
